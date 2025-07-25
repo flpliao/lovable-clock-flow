410a01f1ca1f815970aac04c175511566cc2db64
loneight <aeaw0608@gmail.com>
2025-07-25 14:47:01 +0800

    fix: 修正 DashboardLayout 未登入時內容不渲染問題
    - 將 DashboardLayout 外層加上 ProtectedRoute，確保只有登入狀態才渲染內容
    - 避免未登入時頁面內容空白或異常
    - 提升路由保護一致性與用戶體驗

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import ProtectedRoute from '@/components/common/ProtectedRoute';
 import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 import { protectedRoutes, publicRoutes } from '@/routes/routes';
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 
 // 佈局組件定義
@@ -26,27 +26,32 @@ const AuthLayout: React.FC = () => {
 
 const AppRoutes: React.FC = () => {
   return (
-    <Suspense fallback={<div>載入中...</div>}>
-      <Routes>
-        {/* 主要頁面路由 - 使用 DashboardLayout */}
-        <Route path="/" element={<DashboardLayout />}>
-          {protectedRoutes.map((route, id) => (
-            <Route
-              path={route.path}
-              element={<ProtectedRoute>{<route.component />}</ProtectedRoute>}
-              key={id}
-            />
-          ))}
-        </Route>
+    <Routes>
+      {/* 主要頁面路由 - 使用 DashboardLayout */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        {protectedRoutes.map((route, id) => (
+          <Route
+            path={route.path}
+            element={<ProtectedRoute>{<route.component />}</ProtectedRoute>}
+            key={id}
+          />
+        ))}
+      </Route>
 
-        {/* 認證路由 - 使用 AuthLayout */}
-        <Route path="/" element={<AuthLayout />}>
-          {publicRoutes.map((route, id) => (
-            <Route path={route.path} element={<route.component />} key={id} />
-          ))}
-        </Route>
-      </Routes>
-    </Suspense>
+      {/* 認證路由 - 使用 AuthLayout */}
+      <Route path="/" element={<AuthLayout />}>
+        {publicRoutes.map((route, id) => (
+          <Route path={route.path} element={<route.component />} key={id} />
+        ))}
+      </Route>
+    </Routes>
   );
 };
 
